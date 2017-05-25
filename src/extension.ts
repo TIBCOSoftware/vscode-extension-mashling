3745bf33583a22d5488ba52ae3b718395044eb8b
APAC\tbhatia <tbhatia@tibco.com>
2017-05-25 20:34:01 +0530

    Added following functionality: 1) new commands to create, install and update mashling app. 2) added default snippets for different subsections in mashling.json

--- src/extension.ts
+++ src/extension.ts
@@ -8,14 +8,24 @@ import { MASHLING_MODE } from './MashlingMode';
 import { MashlingCompletionItemProvider } from './MashlingCompletions';
 
 export function activate(context: ExtensionContext) {
+	//get terminal window
+	let terminal = vscode.window.createTerminal('mashling-cli');
 	//Register all the commands
 
 	registerCommands();
-	
+
 	function registerCommands() {
 
-		let disposable = vscode.commands.registerCommand('mashling.createGateway', function () {
+		var disposable = vscode.commands.registerCommand('mashling.createGateway', function () {
 			createGateway();
+		});
+
+		var disposable = vscode.commands.registerCommand('mashling.installMashling', function () {
+			installMashling();
+		});
+
+		var disposable = vscode.commands.registerCommand('mashling.updateMashling', function () {
+			updateMashling();
 		});
 		// Push the disposable to the context's subscriptions
 		context.subscriptions.push(disposable);
@@ -23,11 +33,24 @@ export function activate(context: ExtensionContext) {
 
 	function createGateway() {
 
-		let terminal = vscode.window.createTerminal('mashling-cli');
 		let activeDocPath = vscode.window.activeTextEditor.document.uri.path;
-		activeDocPath = activeDocPath.substring(1,activeDocPath.length);
+		activeDocPath = activeDocPath.substring(1, activeDocPath.length);
+		let appName = vscode.window.showInputBox({ prompt: 'Enter the mashling app name' }).then(val => runCommand(val));
+		function runCommand(appName) {
+			terminal.show(true);
+			terminal.sendText('mashling create ' + appName);
+		};
+
+	}
+
+	function installMashling() {
 		terminal.show(true);
-		terminal.sendText('copy ' + activeDocPath + ' C:/Users/tbhatia/mashling-extensions/vscode-mashling-extension/test.json');
+		terminal.sendText("go get github.com/TIBCOSoftware/mashling-cli/...");
+	}
+
+	function updateMashling() {
+		terminal.show(true);
+		terminal.sendText("go get -u github.com/TIBCOSoftware/mashling-cli/...");
 	}
 	// context.subscriptions.push(vscode.languages.registerHoverProvider(MASHLING_MODE, new MashlingHoverProvider()));
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(MASHLING_MODE, new MashlingCompletionItemProvider(), ':', '\"'));
