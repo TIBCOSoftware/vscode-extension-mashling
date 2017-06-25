403cf49268137b5aad1fcc348229ad009faf9a40
APAC\tbhatia <tbhatia@tibco.com>
2017-06-25 22:36:45 +0530

    Updated Intellisense to contain relevant suggestions at different levels in yaml files.(in progress)

--- client/src/MashlingCompletionDefinitions.ts
+++ client/src/MashlingCompletionDefinitions.ts
@@ -1,79 +1,188 @@
-import { CompletionItemKind } from 'vscode';
+import { CompletionItemKind, CompletionItem } from 'vscode';
 
-export const suggestionsObject = [
-		{
-			label: '\"Test\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"UserRESTTrigger\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"GetUserSuccessHandler\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"limit\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"log\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"invoke\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"reply\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"error\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"UserErrorHandler\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"kafkaConfig\"',
-			kind: CompletionItemKind.Value,
-			data: "name"
-		},
-		{
-			label: '\"string\"',
-			kind: CompletionItemKind.Value,
-			data: "type"
-		},
-		{
-			label: '\"boolean\"',
-			kind: CompletionItemKind.Value,
-			data: "type"
-		},
-		{
-			label: '\"error\"',
-			kind: CompletionItemKind.Value,
-			data: "type"
-		},
-		{
-			label: '\"json\"',
-			kind: CompletionItemKind.Value,
-			data: "type"
-		},
-		{
-			label: '[]',
-			kind: CompletionItemKind.Value,
-			data: "actions"
-		}
-	];
+export const suggestionsObject:CompletionItem[] = [
+	{
+		label: 'gateway',
+		kind: CompletionItemKind.Property,
+		detail: "0gateway",
+		insertText: 'gateway:\n  ',
+		documentation: 'Gateway Object',
+		filterText: 'gateway'
+	},
+	{
+		label: 'name',
+		kind: CompletionItemKind.Property,
+		detail: "name",
+		insertText: 'name: microgateway\n',
+		documentation: 'name of the gateway',
+		filterText: '1'
+	},
+	{
+		label: 'version',
+		kind: CompletionItemKind.Property,
+		detail: "version",
+		insertText: 'version: 0.0.1\n',
+		documentation: 'version number',
+		filterText: '1'
+	},
+	{
+		label: 'description',
+		kind: CompletionItemKind.Property,
+		detail: "description",
+		insertText: 'description: gateway description\n',
+		documentation: 'description of the gateway',
+		filterText: '1'
+	},
+	{
+		label: 'configurations',
+		kind: CompletionItemKind.Property,
+		detail: "1configurations",
+		insertText: 'configurations: \n- name: kafkaConfig\n  type: github.com/TIBCOSoftware/flogo-contrib/config/kafkaConfig\n  description: Configuration for kafka cluster\n  settings:\n  brokers:\n  - localhost:9092\n  - localhost:9093\n  userName: admin\n  password: admin',
+		documentation: 'configurations',
+		filterText: '1'
+	},
+	{
+		label: 'name',
+		kind: CompletionItemKind.Property,
+		detail: "configuration name",
+		insertText: 'name: kafkaConfig\n',
+		documentation: 'configuration name',
+		filterText: 'configurations'
+	},
+	{
+		label: 'type',
+		kind: CompletionItemKind.Property,
+		detail: "configuration type",
+		insertText: 'type: github.com/TIBCOSoftware/flogo-contrib/config/kafkaConfig\n',
+		documentation: 'configuration type',
+		filterText: 'configurations'
+	},
+	{
+		label: 'description',
+		kind: CompletionItemKind.Property,
+		detail: "configuration description",
+		insertText: 'description: Configuration for kafka cluster\n',
+		documentation: 'configuration description',
+		filterText: 'configurations'
+	},
+	{
+		label: 'settings',
+		kind: CompletionItemKind.Property,
+		detail: "configuration settings",
+		insertText: 'settings:\n  ',
+		documentation: 'configuration description',
+		filterText: 'configurations'
+	},
+	{
+		label: 'triggers',
+		kind: CompletionItemKind.Property,
+		detail: "triggers",
+		insertText: 'triggers: \n- ',
+		documentation: 'triggers',
+		filterText: '1'
+	},
+	{
+		label: 'name',
+		kind: CompletionItemKind.Property,
+		detail: "2triggers",
+		insertText: 'name: OrdersTrigger\n',
+		documentation: 'name of the trigger',
+		filterText: 'triggers'
+	},
+	{
+		label: 'type',
+		kind: CompletionItemKind.Property,
+		detail: "type",
+		insertText: 'type: github.com/TIBCOSoftware/flogo-contrib/trigger/kafkaConsumer\n',
+		documentation: 'type of the trigger',
+		filterText: 'triggers'
+	},
+	{
+		label: 'description',
+		kind: CompletionItemKind.Property,
+		detail: "description",
+		insertText: 'description: The trigger on \'orders\' topic\n',
+		documentation: 'description of the trigger',
+		filterText: 'triggers'
+	},
+	{
+		label: 'settings',
+		kind: CompletionItemKind.Property,
+		detail: "settings",
+		insertText: 'settings: \n  topic: orders\n  config: ""',
+		documentation: 'settings of the trigger',
+		filterText: 'triggers'
+	},
+	{
+		label: 'event_links',
+		kind: CompletionItemKind.Property,
+		detail: "event_links",
+		insertText: 'event_links: \n- ',
+		documentation: 'event_links',
+		filterText: '1'
+	},
+	{
+		label: 'trigger',
+		kind: CompletionItemKind.Property,
+		detail: "event_links trigger",
+		insertText: 'trigger: OrdersTrigger\n',
+		documentation: 'event_links trigger',
+		filterText: 'event_links'
+	},
+	{
+		label: 'success_paths',
+		kind: CompletionItemKind.Property,
+		detail: "event_links success_paths",
+		insertText: 'success_paths:\n- ',
+		documentation: 'event_links success_paths',
+		filterText: 'event_links'
+	},
+	{
+		label: 'error_paths',
+		kind: CompletionItemKind.Property,
+		detail: "event_links error_paths",
+		insertText: 'error_paths:\n- ',
+		documentation: 'event_links error_paths',
+		filterText: 'event_links'
+	},
+	{
+		label: 'event_handlers',
+		kind: CompletionItemKind.Property,
+		detail: "event_handlers",
+		insertText: 'event_handlers: \n- ',
+		documentation: 'event_handlers',
+		filterText: '1'
+	},
+	{
+		label: 'name',
+		kind: CompletionItemKind.Property,
+		detail: "name",
+		insertText: 'name: OrderSuccessHandler\n',
+		documentation: 'name',
+		filterText: 'event_handlers'
+	},
+	{
+		label: 'params',
+		kind: CompletionItemKind.Property,
+		detail: "params",
+		insertText: 'params:\n  ',
+		documentation: 'Event Handlers params',
+		filterText: 'event_handlers'
+	},
+	{
+		label: 'description',
+		kind: CompletionItemKind.Property,
+		detail: "description",
+		insertText: 'description: Handle the order processing\n',
+		documentation: 'Event Handlers description',
+		filterText: 'event_handlers'
+	},
+	{
+		label: 'reference',
+		kind: CompletionItemKind.Property,
+		detail: "Event Handlers reference",
+		insertText: 'reference: github.com/TIBCOSoftware/mashling/flows/OrdersSuccessHandler/flow.json\n  ',
+		documentation: 'Event Handlers reference',
+		filterText: 'event_handlers'
+	}
+];
--- client/src/MashlingCompletions.ts
+++ client/src/MashlingCompletions.ts
@@ -3,31 +3,142 @@ import { suggestionsObject } from './MashlingCompletionDefinitions';
 import { CompletionItemKind } from 'vscode';
 import { removeQuotesfromString, removeWhiteSpacesfromString } from './utils';
 
-export class MashlingCompletionItemProvider implements vscode.CompletionItemProvider{
-    public provideCompletionItems(
-        document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
-        Thenable<vscode.CompletionItem[]> {
-			// return suggestionsObject;
-            return GetCompletionsInfo(document, position).then(completionInfo => {
-				return completionInfo;
-		    }, () => {
+export class MashlingCompletionItemProvider implements vscode.CompletionItemProvider {
+	public provideCompletionItems(
+		document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
+		Thenable<vscode.CompletionItem[]> {
+		return GetCompletionsInfo(document, position).then(completionInfo => {
+			return completionInfo;
+		}, () => {
 			return null;
 		});
-   	 }
+	}
 }
 
 export function GetCompletionsInfo(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.CompletionItem[]> {
-    let wordRange = document.getWordRangeAtPosition(position);
+	let wordRange = document.getWordRangeAtPosition(position);
 	let lineText = document.lineAt(position.line).text;
-	
 	let filteredSuggestions;
-	if(lineText.indexOf(':') == -1){
-		// filteredSuggestions = suggestionsObject.filter(n=>n.kind == CompletionItemKind.Property);
-	} else {
-		let propertyName = removeQuotesfromString(lineText.split(':')[0]);
-		propertyName = removeWhiteSpacesfromString(propertyName);
-		filteredSuggestions = suggestionsObject.filter(n => n.data == propertyName);
-	}
+	filteredSuggestions = computePropertyIndentation(document, position);
+	// if (lineText.indexOf(':') == -1) {
+	// 	filteredSuggestions = suggestionsObject.filter(n => n.kind == 9);
+	// } else {
+	// 	let propertyName = removeQuotesfromString(lineText.split(':')[0]);
+	// 	propertyName = removeWhiteSpacesfromString(propertyName);
+	// 	// console.log("propertyName ", propertyName);
+	// 	filteredSuggestions = suggestionsObject.filter(n => n.label == propertyName);
+	// }
 
 	return Promise.resolve(filteredSuggestions);
 }
+
+function computePropertyIndentation(doc: vscode.TextDocument, pos: vscode.Position) {
+	let level;
+	let lineText = textAtLine(doc, pos.line);
+	console.log("Line number ", pos.line);
+	//Check if first line
+	if (pos.line == 0) {
+		level = 0;
+		return suggestionsObject.filter(n => parseInt(n.detail[0]) == 0);
+	}
+	// console.log("space count ", lineText.search(/\S|$/));
+	//line 2
+	if (pos.line == 1 && lineText.search(/\S|$/) == lineText.length && lineText.length == 2) {
+		level = 1;
+		return suggestionsObject.filter(n => parseInt(n.filterText) == 1 && n.kind == 9);
+	}
+	//Not first line and empty line
+	if (pos.line > 1) {
+		//compute Level
+		//If empty line
+		if (lineText.length == 0) {
+			let previousLineText = textAtLine(doc, pos.line - 1);
+			level = computePropertyLevel(previousLineText.search(/\S|$/));
+		}
+		else if (lineText.search(/\S|$/) == lineText.length && lineText.length == 2) {
+			level = computePropertyLevel(lineText.search(/\S|$/));
+			let previousProperties = getAllPropertiesAtLevel1(doc);
+			return suggestionsObject.filter(n => parseInt(n.filterText) == level && n.kind == 9 && previousProperties.indexOf(n.label) == -1);
+		}
+		else if (lineText.match(/[ -]+/i)[0].length === lineText.length && lineText.length > 2) {
+			let parentProperty = getParentProperty(doc, pos.line);
+			console.log("parent property ", parentProperty);
+			let otherPropertiesAtSameLevel = getOtherPropertiesAtSameLevel(doc, pos.line);
+			return suggestionsObject.filter(n => n.filterText == parentProperty && otherPropertiesAtSameLevel.indexOf(n.label) == -1);
+		}
+
+	}
+
+}
+
+
+function textAtLine(doc, lineNumber): String {
+	return doc.lineAt(lineNumber).text;
+}
+
+function computePropertyLevel(spaces) {
+	let level;
+	switch (spaces) {
+		case 2:
+			level = 1
+			break;
+		case 4:
+			level = 2
+			break;
+		case 6:
+			level = 3
+			break;
+		default:
+			level = 0;
+	}
+	return level;
+}
+
+function getAllPropertiesAtLevel1(doc: vscode.TextDocument): String[] {
+	var res = [];
+	for (let i = doc.lineCount - 1; i >= 1; i--) {
+		res.push(textAtLine(doc, i).trim().split(":")[0]);
+	}
+	return res.filter(Boolean);
+};
+
+function getOtherPropertiesAtSameLevel(doc: vscode.TextDocument, lineNumber): String[] {
+	var res = [];
+	let currentPropertylevel = computePropertyLevel(textAtLine(doc, lineNumber).match(/[ -]+/i)[0].length);
+	//Check for previous properties at same level
+	let newPrevLevel = currentPropertylevel;
+	let i = lineNumber - 1;
+	while (newPrevLevel >= currentPropertylevel) {
+		if (newPrevLevel == currentPropertylevel) {
+			res.push(textAtLine(doc, i).match(/[^ -]+/i)[0].split(":")[0]);
+		}
+		newPrevLevel = computePropertyLevel(textAtLine(doc, i - 1).match(/[ -]+/i)[0].length);
+		i--;
+	}
+	//Check for next properties at same level
+	// let newNextLevel = currentPropertylevel;
+	// let j = lineNumber + 1;
+	// while (newNextLevel >= currentPropertylevel) {
+	// 	console.log("while loop 2");
+	// 	console.log("lineText length ", textAtLine(doc, j).length);
+	// 	if (newNextLevel == currentPropertylevel) {
+	// 		res.push(textAtLine(doc, j).match(/[^ -]+/i)[0].split(":")[0]);
+	// 		console.log("res ", res);
+	// 	}
+	// 	newNextLevel = computePropertyLevel(textAtLine(doc, j + 1).match(/[ -]+/i)[0].length);
+	// 	console.log("new next level ", newNextLevel);
+	// 	j++;
+	// }
+	return res;
+}
+
+function getParentProperty(doc: vscode.TextDocument, lineNumber): String {
+	let currentPropertylevel = computePropertyLevel(textAtLine(doc, lineNumber).match(/[ -]+/i)[0].length);
+	let newLevel = currentPropertylevel;
+	let i = lineNumber;
+	while (newLevel >= currentPropertylevel) {
+		newLevel = computePropertyLevel(textAtLine(doc, i - 1).match(/[ -]+/i)[0].length);
+		i--;
+	}
+	return textAtLine(doc, i).match(/[^ -]+/i)[0].split(":")[0];
+};
